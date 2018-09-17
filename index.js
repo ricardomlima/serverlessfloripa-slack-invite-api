5cbe8affbcb9f9f2643c8cf8e410bdece27a9bd7
Ricardo M. Lima <ricardolima89@gmail.com>
2018-09-16 23:14:43 -0300

    making request to slack api via backend

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 'use strict';
 
-exports.handleInviteRequest = (request, response) => {
-  response.status(200).send('Hello World!');
+const got = require('got');
+
+exports.handleInviteRequest = (req, res) => {
+  var email = req.body.email;
+  var url = 'https://slack.com/api/users.admin.invite';
+  var slackToken = 'uc1gsvagA4gSsnC1bEFUwrEg';
+  var formData = JSON.stringify({email: email, token: slackToken});
+  var headers = {'Content-Type':'application/x-www-form-urlencoded'};
+
+  (async () => {
+    const myresponse = await got.post(url, {body:formData, headers:headers});
+    res.status(200).send(myresponse.body);
+  })();
+
 };
 
