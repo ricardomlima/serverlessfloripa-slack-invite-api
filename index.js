14726ce7ea812febb8e16f1cfa88677067bdabc4
Ricardo M. Lima <ricardolima89@gmail.com>
2018-09-25 20:27:18 -0300

    Handling error and success

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 'use strict';
 
 const request = require('request');
+const url = 'https://serverlessfloripa.slack.com';
 
 exports.handleInviteRequest = (req, res) => {
   var email = req.body.email;
-  var url = 'https://serverlessfloripa.slack.com/api/users.admin.invite';
   var slackToken = 'xoxp-416775999249-416776000641-441300168931-a2d7deee1de644df437fb3f3be670b0a';
   var formData = {email: email, token: slackToken, set_active:true};
+  var inviteEndpoint = url + '/api/users.admin.invite';
 
   request.post({
-    url: url,
+    url: inviteEndpoint,
     form: formData
-  }, function(err, httpResponse, body){
-    res.status(200).send(body);
+  }, (err, httpResponse, body) => {
+    if (err) { res.status(500).send('Error ' + err); }
+
+    let parsedBody = JSON.parse(body);
+
+    if (parsedBody.ok === true) {
+      res.status(200).send(body);
+    } else {
+      let error = parsedBody.error;
+      let errorMsg = 'Entre em contato com o administrador do canal: ricardolima89@gmail.com';
+      if (error === 'already_invited'){
+        errorMsg = 'Você já foi convidado! Verifique seu e-mail.';
+      } else if (error === 'already_in_team'){
+        errorMsg = 'Você já faz parte do canal. Visite '+ url;
+      } else if (error === 'invalid_email'){
+        errorMsg = 'Digite um email válido :) ';
+      }
+      res.status(400).send(errorMsg);
+    }
   });
 };
 
