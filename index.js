b27e71e05c9276155f57b00e7d8517dac158c9e7
Ricardo M. Lima <ricardolima89@gmail.com>
2018-10-13 16:17:38 -0300

    refactoring & using env variable for slack token

--- index.js
+++ index.js
@@ -2,32 +2,31 @@
 
 const request = require('request');
 const url = 'https://serverlessfloripa.slack.com';
+const inviteEndpoint = url + '/api/users.admin.invite';
+const slackToken = process.env.SLACK_TOKEN;
 
 exports.handleInviteRequest = (req, res) => {
-  var email = req.body.email;
-  var slackToken = process.env.SLACK_TOKEN;
-  var formData = {email: email, token: slackToken, set_active:true};
-  var inviteEndpoint = url + '/api/users.admin.invite';
+  const requestBody = JSON.parse(req.body);
+  const email = requestBody.email;
+  const formData = {email: email, token: slackToken, set_active:true};
 
   request.post({
     url: inviteEndpoint,
     form: formData
   }, (err, httpResponse, body) => {
     if (err) { res.status(500).send('Error ' + err); }
-
-    let parsedBody = JSON.parse(body);
-
+    const parsedBody = JSON.parse(body);
     if (parsedBody.ok === true) {
       res.status(200).send(body);
     } else {
-      let error = parsedBody.error;
-      let errorMsg = 'Entre em contato com o administrador do canal: ricardolima89@gmail.com';
+      const error = parsedBody.error;
+      let errorMsg = 'Get in touch with our channel admin: ricardolima89@gmail.com';
       if (error === 'already_invited'){
-        errorMsg = 'Você já foi convidado! Verifique seu e-mail.';
+        errorMsg = 'You were already invited! check your e-mail.';
       } else if (error === 'already_in_team'){
-        errorMsg = 'Você já faz parte do canal. Visite '+ url;
+        errorMsg = 'You are already part of our Slack channel. Visit '+ url;
       } else if (error === 'invalid_email'){
-        errorMsg = 'Digite um email válido :) ';
+        errorMsg = 'Please type a valid email :) ';
       }
       res.status(400).send(errorMsg);
     }
